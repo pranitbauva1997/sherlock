6aa3c4877ed63a57af893557003a361e5cfbb88e
Pranit Bauva <pranit.bauva@gmail.com>
2016-01-15 15:46:31 +0530

    Use IRremote.h library for receiving IR signals

---
+++ IR_receive.ino
@@ -0,0 +1,37 @@
+/* and his instructable listed below, IR Remote Controlled Color Changing Cloud (Arduino) 
+ * http://www.instructables.com/id/IR-Remote-Controlled-Color-Changing-Cloud-Arduino/  
+ 
+ 
+ * Lets get started:
+ 
+ The IR sensor's pins are attached to Arduino as so:
+ Pin 1 to Vout (pin 11 on Arduino)
+ Pin 2 to GND
+ Pin 3 to Vcc (+5v from Arduino)
+
+*/
+
+/*******************CODE BEGINS HERE********************/
+
+#include <IRremote.h>
+
+int IRpin = 11;
+IRrecv irrecv(IRpin);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) 
+    {
+      Serial.println(results.value, DEC); // Print the Serial 'results.value'
+      irrecv.resume();   // Receive the next value
+    }
+  
+  
+}
